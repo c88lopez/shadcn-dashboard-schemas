8f8d80ecf4cbca7de1734b3b442c4bf57ac30bf1
Cristian Lopez <cristian@getrentcheck.com>
2025-03-12 14:39:59 -0300

    added Team and package namespace

---
+++ src/Team.ts
@@ -0,0 +1,19 @@
+import { z } from 'zod';
+
+export const TeamSchema = z.object({
+  cuid: z.string(),
+  name: z
+    .string()
+    .min(2, { message: 'team name must be at least 2 characters' })
+    .max(20, { message: 'team name must be at most 20 characters' }),
+});
+
+export const TeamCreateSchema = z.object({
+  username: TeamSchema.shape.name,
+});
+
+export const TeamUpdateSchema = z.object({
+  username: TeamSchema.shape.name,
+});
+
+export type Team = z.infer<typeof TeamSchema>;
