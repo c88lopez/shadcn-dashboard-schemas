d821888d2fea7d528aeaa63face5a1b126d9d710
Cristian Lopez <cristian@getrentcheck.com>
2025-03-28 02:54:29 -0300

    better error reporting

--- src/Player.ts
+++ src/Player.ts
@@ -3,17 +3,41 @@ import { z } from 'zod';
 const basePlayerSchema = z.object({
   cuid: z.string().cuid(),
   firstName: z
-    .string()
+    .string({
+      required_error: 'first name is required',
+      invalid_type_error: 'first name must be a string',
+    })
     .min(2, { message: 'first name must be at least 2 characters' }),
   lastName: z
-    .string()
+    .string({
+      required_error: 'last name is required',
+      invalid_type_error: 'last name must be a string',
+    })
     .min(2, { message: 'last name must be at least 2 characters' }),
-  dateOfBirth: z.string().date().optional(),
+  dateOfBirth: z.number().optional(),
   phoneNumber: z.string().optional(),
   email: z.string().email().optional(),
   instagram: z.string().url().optional(),
 });
 
 export const PlayerSchema = basePlayerSchema;
+
+export const CreatePlayerSchema = z.object({
+  firstName: basePlayerSchema.shape.firstName,
+  lastName: basePlayerSchema.shape.lastName,
+  dateOfBirth: basePlayerSchema.shape.dateOfBirth,
+  phoneNumber: basePlayerSchema.shape.phoneNumber,
+  email: basePlayerSchema.shape.email,
+  instagram: basePlayerSchema.shape.instagram,
+});
+
+export const UpdatePlayerSchema = z.object({
+  firstName: basePlayerSchema.shape.firstName.optional(),
+  lastName: basePlayerSchema.shape.lastName.optional(),
+  dateOfBirth: basePlayerSchema.shape.dateOfBirth,
+  phoneNumber: basePlayerSchema.shape.phoneNumber,
+  email: basePlayerSchema.shape.email,
+  instagram: basePlayerSchema.shape.instagram,
+});
 
 export type Player = z.infer<typeof basePlayerSchema>;
